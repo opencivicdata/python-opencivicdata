931ccfaff8ebd0396cd5740c4c07f9143e13d8b3
James Turk <james.p.turk@gmail.com>
2014-05-02 01:22:12 -0400

    createdb in travis

--- opencivicdata/tests/test_settings.py
+++ opencivicdata/tests/test_settings.py
@@ -4,7 +4,7 @@ INSTALLED_APPS = ('opencivicdata',)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'testdb',
+        'NAME': 'test',
         'USER': 'test',
         'PASSWORD': 'test',
         'HOST': 'localhost',
