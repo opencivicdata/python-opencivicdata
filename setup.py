cfe1b0e5971898c0f857ebb6ad74cf0466ccf541
James Turk <james.p.turk@gmail.com>
2014-06-17 17:04:54 -0400

    0.4.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="opencivicdata",
-      version='0.4.2',
+      version='0.4.3',
       py_modules=['opencivicdata'],
       author="James Turk",
       author_email='jturk@sunlightfoundation.com',
