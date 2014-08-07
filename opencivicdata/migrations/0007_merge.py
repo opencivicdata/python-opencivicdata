dccd24a7921ef0c1163db107d14e1508e29a04f5
twneale <twneale@gmail.com>
2014-08-07 16:10:20 -0400

    Merged admin migrations with 338a7707b93d85db902147764d40dc021fcb202f, which renamed Division.display_name to Division.name

---
+++ opencivicdata/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('opencivicdata', '0005_auto_20140731_1101'),
+        ('opencivicdata', '0006_auto_20140729_1600'),
+    ]
+
+    operations = [
+    ]
