9400eb726c640cbae86d3f1c514ee4013cbf1fbc
twneale <twneale@gmail.com>
2014-08-07 16:11:21 -0400

    Added Membership and Person admin improvements.

--- opencivicdata/admin/people_orgs.py
+++ opencivicdata/admin/people_orgs.py
@@ -1,5 +1,7 @@
+from django.core import urlresolvers
 from django.contrib import admin
 from opencivicdata.models import people_orgs as models
+
 from opencivicdata.admin.base import (
     IdentifierInline, LinkAdmin
     )
@@ -52,9 +54,47 @@ class PostLinkAdmin(admin.ModelAdmin):
     pass
 
 
+class PersonMembershipInline(admin.TabularInline):
+    model = models.Membership
+    readonly_fields = ('organization', 'post')
+    fields = ('organization', 'post', 'label', 'role')
+    extra = 0
+
 @admin.register(models.Person)
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ['name']
+    fields = (
+        'name', 'id', 'image',
+        ('birth_date', 'death_date'),
+        'biography', 'extras',
+        ('gender', 'national_identity', 'sort_name', 'summary'))
+
+    list_selected_related = (
+        'memberships',
+        )
+    inlines = [PersonMembershipInline]
+
+    def get_memberships(self, obj):
+        memberships = obj.memberships.select_related('organization__jurisdiction')
+        html = []
+        SHOW_N = 5
+        for memb in memberships[:SHOW_N]:
+            info = (memb._meta.app_label, memb._meta.module_name)
+            admin_url = urlresolvers.reverse('admin:%s_%s_change' % info, args=(memb.pk,))
+            tmpl = '<a href="%s">%s %s</a>\n'
+            html.append(tmpl % (
+                admin_url,
+                memb.organization.jurisdiction.name,
+                memb.organization.name))
+        more = len(memberships) - SHOW_N
+        if 0 < more:
+            html.append('And %d more' % more)
+        return '<br/>'.join(html)
+
+    get_memberships.short_description = 'Memberships'
+    get_memberships.allow_tags = True
+
+    list_display = ('name', 'id', 'get_memberships')
 
 
 @admin.register(models.PersonIdentifier)
@@ -84,8 +124,11 @@ class PersonSourceAdmin(LinkAdmin):
 
 @admin.register(models.Membership)
 class MembershipAdmin(admin.ModelAdmin):
+    readonly_fields = ('organization', 'person', 'post', 'on_behalf_of')
     list_display = ('organization', 'person', 'post', 'on_behalf_of',
         'label', 'role', 'start_date', 'end_date',)
+    fields = ('organization', 'person', 'role', 'post', 'on_behalf_of',
+        'label', ('start_date', 'end_date'), 'extras')
 
 
 @admin.register(models.MembershipContactDetail)
