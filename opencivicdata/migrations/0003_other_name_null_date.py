b6eccfcb3d4e73c5c22d4681d2415201bf7d7cfb
Bob Lannon <blannon@gmail.com>
2015-04-24 16:50:45 -0400

    bumping migrations to make room for new 0002 auto

--- opencivicdata/migrations/0003_other_name_null_date.py
+++ opencivicdata/migrations/0003_other_name_null_date.py
@@ -7,6 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
+        #TODO change to depend on auto 0002
         ('opencivicdata', '0001_initial'),
     ]
 
