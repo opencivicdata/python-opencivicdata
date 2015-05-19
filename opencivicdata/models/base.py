6a22bc9347cc579d2cee2a2f85765980f044f2bd
James Turk <james.p.turk@gmail.com>
2015-05-19 12:33:06 -0400

    Revert "start removing uuidfield"
    This reverts commit c976617f641f3a7f6354ae83126bc5b2b998633e.

--- opencivicdata/models/base.py
+++ opencivicdata/models/base.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.core.validators import RegexValidator
 from jsonfield import JSONField
+from uuidfield import UUIDField
 from .. import common
 
 
