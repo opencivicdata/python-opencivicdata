dc5092708a7f6ff52685c3cb1a59a99aae660239
James Turk <james.p.turk@gmail.com>
2015-09-24 23:44:01 -0400

    0.7.0a3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="opencivicdata-django",
-      version='0.7.0a2',
+      version='0.7.0a3',
       author="James Turk",
       author_email='james.p.turk@gmail.com',
       license="BSD",
