def31377d8bd8a681b601307469ed6c7f174a69f
James Gordon <gordon.je@gmail.com>
2017-03-08 15:16:39 -0600

    Add backports.csv and source code encoding

--- opencivicdata/divisions/__init__.py
+++ opencivicdata/divisions/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import os
 import re
 import io
 import csv
+from backports import csv
 
 PWD = os.path.abspath(os.path.dirname(__file__))
 OCD_DIVISION_CSV = os.environ.get('OCD_DIVISION_CSV', os.path.join(PWD, 'division-ids/identifiers/country-{}.csv'))
--- opencivicdata/divisions/tests/test_division.py
+++ opencivicdata/divisions/tests/test_division.py
@@ -1,16 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys
 import pytest
 from .. import Division
 
 
-@pytest.mark.skipif(sys.version_info < (3,3), reason="requires python3.3")
 def test_get():
     wake = Division.get('ocd-division/country:us/state:nc/county:wake')
     assert wake.name == 'Wake County'
     assert wake.name in str(wake)
 
 
-@pytest.mark.skipif(sys.version_info < (3,3), reason="requires python3.3")
 def test_children():
     us = Division.get('ocd-division/country:us')
     assert len(list(us.children('state', duplicates=False))) == 50
