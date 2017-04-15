ead1be64e2c74a4707f1fc401f7e424d232a6f8b
James Turk <james.p.turk@gmail.com>
2017-04-15 17:50:03 -0400

    add Django 1.11, drop 1.9, add 3.6

--- setup.py
+++ setup.py
@@ -34,5 +34,6 @@ setup(name="opencivicdata-django",
                    "Programming Language :: Python :: 2.7",
                    "Programming Language :: Python :: 3.4",
                    "Programming Language :: Python :: 3.5",
+                   "Programming Language :: Python :: 3.6",
                    ],
       )
