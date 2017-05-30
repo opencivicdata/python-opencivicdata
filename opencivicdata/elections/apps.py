7de5bfda9f148163bfaab09019619abf968737b1
James Gordon <gordon.je@gmail.com>
2017-05-30 09:38:57 -0500

    Move elections stuff into its own app

---
+++ opencivicdata/elections/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+import os
+
+
+class BaseConfig(AppConfig):
+    name = 'opencivicdata.elections'
+    verbose_name = 'Open Civic Data - Elections'
+    path = os.path.dirname(__file__)
