ddbefe2684cfc375e47c2ef1763bb1214a822f02
James Gordon <gordon.je@gmail.com>
2017-06-05 12:19:12 -0500

    Add import of RetentionContestOption

--- opencivicdata/elections/models/__init__.py
+++ opencivicdata/elections/models/__init__.py
@@ -3,11 +3,12 @@ from .election import Election, ElectionIdentifier, ElectionSource
 from .candidacy import Candidacy, CandidacySource
 from .contests.base import ContestBase
 from .contests.ballot_measure import (BallotMeasureContest, BallotMeasureContestOption,
-                                      BallotMeasureContestIdentifier, 
+                                      BallotMeasureContestIdentifier,
                                       BallotMeasureContestSource,
-                                      RetentionContest, RetentionContestIdentifier, 
+                                      RetentionContest, RetentionContestOption,
+                                      RetentionContestIdentifier,
                                       RetentionContestSource)
 from .contests.candidate import (CandidateContest, CandidateContestPost,
                                  CandidateContestIdentifier, CandidateContestSource)
-from .contests.party import (PartyContest, PartyContestOption, 
+from .contests.party import (PartyContest, PartyContestOption,
                              PartyContestIdentifier, PartyContestSource)
