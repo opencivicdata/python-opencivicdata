37cdd5c4ede3a395d60838c73aab3c75b4bb4814
palewire <ben.welsh@gmail.com>
2017-08-16 14:48:14 -0700

    Added help_text to Jurisdiction fields

--- opencivicdata/core/models/jurisdiction.py
+++ opencivicdata/core/models/jurisdiction.py
@@ -11,12 +11,33 @@ from .division import Division
 @python_2_unicode_compatible
 class Jurisdiction(OCDBase):
     id = OCDIDField(ocd_type='jurisdiction')
-    name = models.CharField(max_length=300)
-    url = models.URLField(max_length=2000)
-    classification = models.CharField(max_length=50, choices=JURISDICTION_CLASSIFICATION_CHOICES,
-                                      default='government', db_index=True)
-    feature_flags = ArrayField(base_field=models.TextField(), blank=True, default=list)
-    division = models.ForeignKey(Division, related_name='jurisdictions', db_index=True)
+    name = models.CharField(
+        max_length=300,
+        help_text="The common name of the Jurisdiction, such as 'Wyoming' or 'Hope County School System.'"
+    )
+    url = models.URLField(
+        max_length=2000,
+        help_text="The primary website of the Jurisdiction."
+    )
+    classification = models.CharField(
+        max_length=50,
+        choices=JURISDICTION_CLASSIFICATION_CHOICES,
+        default='government',
+        db_index=True,
+        help_text="The type of jurisdiction being defined."
+    )
+    feature_flags = ArrayField(
+        base_field=models.TextField(),
+        blank=True,
+        default=list,
+        help_text="A list of features that are present for data in this jurisdiction."
+    )
+    division = models.ForeignKey(
+        Division,
+        related_name='jurisdictions',
+        db_index=True,
+        help_text="A link to a Division related to this Jurisdiction."
+    )
 
     class Meta:
         db_table = 'opencivicdata_jurisdiction'
