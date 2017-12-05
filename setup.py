90cc0e587ffdd93c43406eac932bcbedba244611
James Turk <james.p.turk@gmail.com>
2017-12-04 23:36:16 -0500

    bump version post-release

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ else:
     extras_require[":python_version<='2.7'"] = ["backports.csv"]
 
 setup(name="opencivicdata",
-      version='2.1.0',
+      version='2.2.0-dev0',
       author="James Turk",
       author_email='james@openstates.org',
       license="BSD",
