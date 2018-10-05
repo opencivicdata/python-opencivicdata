d9705acfbe70f8179830bda1660ec05eb1d45d02
Forest Gregg <fgregg@uchicago.edu>
2018-10-05 10:27:23 -0500

    longer event names

---
+++ opencivicdata/legislative/migrations/0007_longer_event_name.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('legislative', '0006_billversion_extras'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='name',
+            field=models.CharField(max_length=600),
+        ),
+    ]
