ccdb4787cb3a3b3ba24d606c09075fc3e90f983e
James Turk <dev@jamesturk.net>
2018-11-16 23:24:07 -0500

    remove admin views that never really got fleshed out

--- opencivicdata/core/admin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^opencivicdata/(?P<jur_id>.*)/merge/$', views.merge_tool,
-        name='merge'),
-    url(r'^opencivicdata/merge/confirm/$', views.merge_confirm,
-        name='merge_confirm'),
-]
