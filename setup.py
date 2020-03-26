ad30a91a243fd8784955b2440e79f23ba6fe7512
James Turk <dev@jamesturk.net>
2020-03-26 14:53:38 -0400

    3.2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ extras_require = {
 
 setup(
     name="opencivicdata",
-    version="3.1.0",
+    version="3.2.0",
     author="James Turk",
     author_email="james@openstates.org",
     license="BSD",
