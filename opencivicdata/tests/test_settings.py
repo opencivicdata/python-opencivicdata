74f44367f3c9c7a14337cb2faefed4878c34cdc1
msj <monkruman.st.jules@datamde.us>
2022-12-08 14:31:47 -0500

    Use older coveralls version

--- opencivicdata/tests/test_settings.py
+++ opencivicdata/tests/test_settings.py
@@ -5,12 +5,14 @@ INSTALLED_APPS = (
     "opencivicdata.legislative.apps.BaseConfig",
     "opencivicdata.elections.apps.BaseConfig",
 )
+GDAL_LIBRARY_PATH = '/opt/homebrew/Cellar/gdal/3.5.3_1/lib/libgdal.dylib'
+GEOS_LIBRARY_PATH = '/opt/homebrew/Cellar/geos/3.11.1/lib/libgeos_c.dylib'
 DATABASES = {
     "default": {
         "ENGINE": "django.contrib.gis.db.backends.postgis",
         "NAME": "test",
-        "USER": "test",
-        "PASSWORD": "test",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
         "HOST": "localhost",
     }
 }
